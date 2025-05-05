9015bb97e8744c08628be7dc22972324885604ff
oyatullohm <marziyahoni@gmail.com>
2025-05-05 20:51:44 +0500

    f

--- Product/tasks.py
+++ Product/tasks.py
@@ -18,19 +18,30 @@ def refresh_products_cache():
     response = requests.post(url, auth=HTTPBasicAuth(username, password), stream=True, json={})
     if response.status_code == 200:
         data = response.json().get('array', [])
-        uid_list = [item["UID"] for item in data]
+        uid_list = []
+
+        for item in data:
+            try:
+                uid = int(item.get("UID"))
+                uid_list.append(uid)
+            except (TypeError, ValueError):
+                continue
+
         products = Product.objects.filter(uid__in=uid_list)
         products_dict = {p.uid: p for p in products}
-        
+
         result = []
         for item in data:
-            # category =  Category.objects.get_or_create(name=item['Class'])
-            product = products_dict.get(int(item["UID"]))
-    
+            try:
+                uid = int(item.get("UID"))
+            except (TypeError, ValueError):
+                continue
+
+            product = products_dict.get(uid)
             if product:
                 result.append({
                     "id": product.id,
-                    "name": item.get("Name", ""),  # agar "Name" yoq bolsa "" qaytaradi
+                    "name": item.get("Name", ""),
                     "price": item.get("Price", 0),
                     "class": item.get("Class", ""),
                     "producer": item.get("Producer", ""),
@@ -39,15 +50,15 @@ def refresh_products_cache():
                     "ReleaseForm": item.get("ReleaseForm", ""),
                     "ProductType": item.get("ProductType", ""),
                     "ExpDate": item.get("ExpDate", ""),
-                    "info":product.info,
-                    "image1": product.image1.url if product.image1 else "",
+                    "info": product.info,
+                     "image1": product.image1.url if product.image1 else "",
                     "image2": product.image2.url if product.image2 else "",
                     "image3": product.image3.url if product.image3 else "",
                 })
 
-        r.setex('final_result', 3600*24, json.dumps(result))
-        print("Redis cache updated successfully!")
-        # print(result)
-        logger.info(f"Redis cache updated successfully! {datetime.datetime.now()}")
+            r.setex('final_result', 3600*24, json.dumps(result))
+            print("Redis cache updated successfully!")
+            # print(result)
+            logger.info(f"Redis cache updated successfully! {datetime.datetime.now()}")
     else:
         logger.error(f"Failed to refresh product data! Status code: {response.status_code}")
--- Product/tests.py
+++ Product/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from requests.auth import HTTPBasicAuth
+# import requests
+# # Create your tests here.
+# url = "http://93.170.11.10:8088/RM_OPT/hs/online/stock"
+# username = "Online"
+# password = "cJXGLytPHb3nDNZf5gRh7jzwa"
+# response = requests.post(url, auth=HTTPBasicAuth(username, password), stream=True, json={})
+# if response.status_code == 200:
+#     data = response.json().get('array', [])
 
-# Create your tests here.
+# print(data)
